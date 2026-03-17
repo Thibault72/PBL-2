809b409a599347f54c0743220fd31cb17b6214f2
mugnierblanche <mugnierblanche@gmail.com>
2026-03-17 19:48:38 +0100

    add aircraft sorting algorithms

--- sorts.py
+++ sorts.py
@@ -4,51 +4,37 @@
 # - Selection sort
 # Both algorithms use a policy function that compares two aircraft and decides wich one has priority.
 
+
 def insertion_sort(aircraft_list, policy):
-    # Create a copy of the list to avoid modifying the original dataset
-    aircraft = aircraft_list.copy()
-    # Loop through the list starting from the second element
-    for i in range(1, len(aircraft)):
-        # Aircraft currently being placed in the sorted part
-        current_aircraft = aircraft[i]
-        j=j-1
-        # Move elements that have lower priority to the right until we find correct position
-        while j>=0 and policy(current_aircraft, aircraft[j]):
-            aircraft[j+1] = aircraft[j]  # shift aircraft to the right
-            j-=1
-        # Insert th aircraft in the correct position
-        aircraft[j+1] = current_aircraft
-    # Return the sorted list
-    return aircraft
+    n = len(aircraft_list)
+    for i in range(1, n):
+        current_aircraft = aircraft_list[i]
+        j = i - 1
+        while j >= 0 and policy(current_aircraft, aircraft_list[j]):
+            aircraft_list[j + 1] = aircraft_list[j]
+            j -= 1
+        aircraft_list[j + 1] = current_aircraft
+    return aircraft_list
 
 
 def selection_sort(aircraft_list, policy):
-        # Copy the list so the original data is not modified
-        aircraft = aircraft_list.copy()
-        # Get the number of aircraft
-        n=len(aircraft)
-        # Move the boundary of the unsorted part of the list
-        for i in range(n):
-             # Assume the first aircraft of the unsorted part has priority
+        n = len(aircraft_list)
+        for i in range(0, n - 1):
              priority_index = i
-             # Search for a higher priority aircraft in the rest of the list
-             for j in range (i + 1, n):
-                  # If aircaft [j] has higher priority, update the index
-                  if policy(aircraft[j], aircraft[priority_index]):
+             for j in range(i + 1, n):
+                  if policy(aircraft_list[j], aircraft_list[priority_index]):
                        priority_index = j
-        # Swap the found aircraft with the first unsorted position
-        aircraft[i], aircraft[priority_index] = aircraft[priority_index], aircraft[i]
-        # Return the sorted list
-        return aircraft
+             aircraft_list[i], aircraft_list[priority_index] = aircraft_list[priority_index], aircraft_list[i]
+        return aircraft_list
         
-        
+
 def sort_aircraft(aircraft_list, policy, method="insertion"):
-     # Choose the sorting algorithm depending o the method parameter
-     if method=="insetion":
+
+     if method=="insertion":
           return insertion_sort(aircraft_list, policy)
      
      elif method=="selection":
           return selection_sort(aircraft_list, policy)
-     # If the method does not exist, raise an error
+     
      else:
           raise ValueError("Unknown sorting method")
